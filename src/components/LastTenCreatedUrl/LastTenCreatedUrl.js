7f52a48a99dc65094fa85b5d99f103b680d8f081
shirin danehpash <shirin.danehpash@ikea.com>
2019-02-18 15:11:06 +0100

    one air is added between const and return

--- src/components/LastTenCreatedUrl/LastTenCreatedUrl.js
+++ src/components/LastTenCreatedUrl/LastTenCreatedUrl.js
@@ -6,6 +6,7 @@ import styles from './LastTenCreatedUrl.module.css';
 export const LastTenCreatedUrl = ({ createdUrls, inputUrls }) => {
   sessionStorage['url'] = createdUrls.slice(-10);
   const hrefs = inputUrls.slice(-10);
+
   return (
     <section className={styles.container}>
       <p>Your last ten created urls</p>
